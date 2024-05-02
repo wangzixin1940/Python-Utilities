1f77d31806c520510ba52aa9804b3bd1cf54f9a0
--global <wangzixin1940@outlook.com>
2024-05-02 15:02:52 +0800

    Update 5/2

--- src/cmdtools/md5.py
+++ src/cmdtools/md5.py
@@ -37,43 +37,106 @@ def get_file_sha1(fname):
 
 def main():
     argvs = sys.argv
-    if len(argvs) > 4 or len(argvs) < 2:
+    if len(argvs) > 5 or len(argvs) < 2:
         print("""使用:
-python / py md5.py [filename] [--check [value]]
+python / py md5.py [filename] [--check [-md5][-sha256][-sha1][value]]
 
 filename : 文件名，比如"1.py"、"HelloWorld.java"等等
 
---check : 检查文件MD5值，比如"--check 1234567890abcdef"
+--check : 检查文件MD5、SHA256、SHA1值，比如"--check -md5 1234567890abcdef"
     value : 原文件MD5值，比如"1234567890abcdef"(一般来源于提供文件的网站或者人)
+    -md5 : 校验文件MD5值
+    -sha256 : 校验文件SHA256值
+    -sha1 : 校验文件SHA1值
+    注意：-md5、-sha256、-sha1不可以同时使用。
 
 错误：参数过多或者过少。
 
 """)
         return 1
-    filename = argvs[1].strip("\"")
+    filename = argvs[1].strip()
     if not os.path.isfile(filename):
         print("错误！文件不存在，请检查文件名和路径！")
         return 2
-    if "--check" in argvs and len(argvs) == 4:
-        value = argvs[3]
-        if get_file_md5(filename) == value:
-            print("文件MD5值正确！")
+    if "--check" in argvs and len(argvs) == 5:
+        if "-md5" in argvs:
+            value = argvs[3]
+            if get_file_md5(filename) == value:
+                print("文件MD5值正确！")
+                print("文件MD5值：",get_file_md5(filename))
+            else:
+                print("文件MD5值错误！")
+                print("文件MD5值：",get_file_md5(filename))
+            return 0
+        elif "-sha256" in argvs:
+            value = argvs[3]
+            if get_file_sha256(filename) == value:
+                print("文件SHA256值正确！")
+                print("文件SHA256值：",get_file_sha256(filename))
+            else:
+                print("文件SHA256值错误！")
+                print("文件SHA256值：",get_file_sha256(filename))
+            return 0
+        elif "-sha1" in argvs:
+            value = argvs[3]
+            if get_file_sha1(filename) == value:
+                print("文件SHA1值正确！")
+                print("文件SHA1值：",get_file_sha1(filename))
+            else:
+                print("文件SHA1值错误！")
+                print("文件SHA1值：",get_file_sha1(filename))
+            return 0
         else:
-            print("文件MD5值错误！")
-        return 0
-    elif "--check" in argvs and len(argvs) == 3:
-        print("""使用:
-python / py md5.py [filename] [--check [value]]
+            print("""使用:
+python / py md5.py [filename] [--check
+ [-md5][-sha256][-sha1][value]]
 
 filename : 文件名，比如"1.py"、"HelloWorld.java"等等
 
---check : 检查文件MD5值，比如"--check 1234567890abcdef"
+--check : 检查文件MD5、SHA256、SHA1值，比如"--check -md5 1234567890abcdef"
     value : 原文件MD5值，比如"1234567890abcdef"(一般来源于提供文件的网站或者人)
+    -md5 : 校验文件MD5值
+    -sha256 : 校验文件SHA256值
+    -sha1 : 校验文件SHA1值
+    注意：-md5、-sha256、-sha1不可以同时使用。
+
+错误：无效参数。
+
+""")
+    elif "--check" in argvs and len(argvs) == 3:
+        print("""使用:
+python / py md5.py [filename] [--check [-md5][-sha256][-sha1][value]]
+
+filename : 文件名，比如"1.py"、"HelloWorld.java"等等
+
+--check : 检查文件MD5、SHA256、SHA1值，比如"--check -md5 1234567890abcdef"
+    value : 原文件MD5值，比如"1234567890abcdef"(一般来源于提供文件的网站或者人)
+    -md5 : 校验文件MD5值
+    -sha256 : 校验文件SHA256值
+    -sha1 : 校验文件SHA1值
+    注意：-md5、-sha256、-sha1不可以同时使用。
 
 错误：缺少必须参数"value"。
 
 """)
         return 3
+    elif len(argvs) > 5:
+        print("""使用:
+python / py md5.py [filename] [--check [-md5][-sha256][-sha1][value]]
+
+filename : 文件名，比如"1.py"、"HelloWorld.java"等等
+
+--check : 检查文件MD5、SHA256、SHA1值，比如"--check -md5 1234567890abcdef"
+    value : 原文件MD5值，比如"1234567890abcdef"(一般来源于提供文件的网站或者人)
+    -md5 : 校验文件MD5值
+    -sha256 : 校验文件SHA256值
+    -sha1 : 校验文件SHA1值
+    注意：-md5、-sha256、-sha1不可以同时使用。
+
+错误：参数过多。
+
+""")
+        return 5
     elif len(argvs) == 2:
         print("文件名：",filename)
         print("文件大小：",os.path.getsize(filename),"字节")
@@ -85,12 +148,16 @@ filename : 文件名，比如"1.py"、"HelloWorld.java"等等
         return 0
     else :
         print("""使用:
-python / py md5.py [filename] [--check [value]]
+python / py md5.py [filename] [--check [-md5][-sha256][-sha1][value]]
 
 filename : 文件名，比如"1.py"、"HelloWorld.java"等等
 
---check : 检查文件MD5值，比如"--check 1234567890abcdef"
+--check : 检查文件MD5、SHA256、SHA1值，比如"--check -md5 1234567890abcdef"
     value : 原文件MD5值，比如"1234567890abcdef"(一般来源于提供文件的网站或者人)
+    -md5 : 校验文件MD5值
+    -sha256 : 校验文件SHA256值
+    -sha1 : 校验文件SHA1值
+    注意：-md5、-sha256、-sha1不可以同时使用。
 
 错误：未知错误或者无效参数
 
