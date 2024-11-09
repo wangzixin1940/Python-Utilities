9f4baf7bfdb516d24305d198f9f4145b60741c3a
Zixin Wang <wangzixin1940@outlook.com>
2024-11-09 21:21:39 +0800

    Re-tested

--- new_main.py
+++ new_main.py
@@ -1,22 +1,25 @@
 from PySide6 import QtWidgets
 from PySide6.QtWidgets import QApplication
-from PySide6.QtUiTools import QUiLoader
-from PySide6.QtCore import QFile, QIODevice
-import qfluentwidgets
+from PySide6.QtGui import QIcon
+from data.ui import zhCN
+import qt_material as Stylesheet
 import sys
+
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = zhCN.Ui_MainWindow()
+        self.ui.setupUi(self)
 
 def main():
     app = QApplication(sys.argv)
-    ui_file = QFile("./data/ui/zh-cn.ui")
-    if not ui_file.open(QIODevice.ReadOnly):
-        print(f"Cannot open UI file: {ui_file.errorString()}")
-        sys.exit(-1)
-    loader = QUiLoader()
-    window = loader.load(ui_file)
-    ui_file.close()
-    if not window:
-        print(loader.errorString())
-        sys.exit(-1)
+    Stylesheet.apply_stylesheet(app, "dark_medical.xml")
+    window = MainWindow()
+    window.setWindowIcon(QIcon("./images/pride.ico"))
+    window.resize(320, 500)
+    window.setFixedSize(320, 500)
     window.show()
     sys.exit(app.exec())
 
